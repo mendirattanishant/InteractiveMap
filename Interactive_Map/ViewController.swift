06679ea9e7d057d406e9a893d17ef71240601b5c
Nishant <nishant.mendiratta89@gmail.com>
2015-11-07 21:00:01 -0800

    Get X-Y coordinates of Image

--- Interactive_Map/ViewController.swift
+++ Interactive_Map/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var testRouch: UILabel!
+//    @IBOutlet var testtouch: [UILabel]!
+    @IBOutlet weak var mapScrollView: UIScrollView!
 
+    @IBOutlet weak var mapImageView: UIImageView!
+//    @IBOutlet var testTouch: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.mapScrollView.minimumZoomScale = 1
+        self.mapScrollView.maximumZoomScale = 5
+        
+        mapImageView.userInteractionEnabled = true // IMPORTANT
+        mapImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "didTapImage:"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func viewForZoomingInScrollView(scrollView: UIScrollView)
+        -> UIView {
+            return self.mapImageView;
+    }
+    func didTapImage(gesture: UIGestureRecognizer) {
+        let point = gesture.locationInView(gesture.view)
+        print(point)
+    }
 
 }
 
